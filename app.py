711151eb8a776d8388c9df244039afe1446afbea
Bonny Biswas <labonny@gmail.com>
2023-01-02 15:03:27 -0500

    Use resnet34 model trained on FER-2013 dataset
    not tested locally

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
+learn = load_learner('model-v4.pkl')
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(48,48)), outputs=gr.outputs.Label(num_top_classes=3))
 iface.launch()
 
