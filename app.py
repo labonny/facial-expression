546b90e1b057cba4eb73e8c11e71d75e29f23c8b
Bonny Biswas <labonny@gmail.com>
2023-01-07 13:00:38 -0500

    Using a new model trained on 224x224 images
    Retrained resnet34 using the same FER203 dataset, but
    resized these images to 224x224 - these are what the original
    model expects, and the accuracy is a bit better, with no data
    cleaning steps.
    
    Also, most images people will upload will be higher resolution
    so using 224x224 pixels will give better results than using 48x48.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import gradio as gr
 from fastai.vision.all import *
 import cv2
+import PIL
 
-learn = load_learner('model-v4.pkl')
+learn = load_learner('fec224-resnet34-v1.pkl')
 labels = learn.dls.vocab
 def predict(img):
     image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY, dstCn=3 )
@@ -12,6 +13,6 @@ def predict(img):
 title = "Facial Expression Classifier"
 description = "A facial expression classifier, trained using the <a href='https://www.kaggle.com/datasets/msambare/fer2013'>FER-2013 dataset</a>. This dataset consists of 28,709 examples of faces: each one is 48x48 grayscale pixels and is labelled with one of the following expressions: anger, disgust, fear, happy, neutral, sad, surprise.<p><p>This was used to train a resnet34 model."
 examples = ["angryExample.jpg", "disgustExample.jpg", "fearExample.jpg", "happyExample.jpg", "neutralExample.jpg", "sadExample.jpg", "surpriseExample.jpg"]
-iface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(48,48)), outputs=gr.outputs.Label(num_top_classes=3), examples=examples, title=title, description=description,interpretation='default')
+iface = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(224,224)), outputs=gr.outputs.Label(num_top_classes=3), examples=examples, title=title, description=description,interpretation='default')
 iface.launch()
 
